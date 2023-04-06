8981a321c1437b1f5a33364b00f19868e20b6af0
iggut <igor.gutchin@gmail.com>
2023-04-06 11:20:30 -0400

    Update unstable.nix

--- system/programs/unstable.nix
+++ system/programs/unstable.nix
@@ -6,20 +6,20 @@
 let
   unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
 in {
+
 	environment.systemPackages = with pkgs; [
-		#(callPackage ./gamescope.nix {}) # Wayland microcompositor
-		(callPackage ./self-built/sfwbar {}) # Status bar for Wayland
-		pkgs.gamescope # Wayland microcompositor
-		google-chrome # Hate it and love it Browser
-		nwg-drawer # Sexy app launcher
-		nwg-menu # Sexy app menu
-		libxkbcommon
-    alsaLib
-		libpulseaudio
-		libgpgerror
-    libgcrypt
-	  libbsd
-		libdrm
+    (callPackage ./self-built/sfwbar {})    # Status bar for Wayland
+    gamescope                               # Wayland microcompositor
+    google-chrome                           # Hate it and love it Browser
+    nwg-drawer                              # Sexy app launcher
+    nwg-menu                                # Sexy app menu
+    libxkbcommon                            # Keyboard handling library for X11 and Wayland
+    alsaLib                                 # ALSA (Advanced Linux Sound Architecture) library
+    libpulseaudio                           # PulseAudio sound server library
+    libgpgerror                             # GnuPG error codes library
+    libgcrypt                               # Cryptographic library for data encryption and decryption
+    libbsd                                  # Library providing useful functions commonly found on BSD systems
+    libdrm                                  # Library for managing DRM (Direct Rendering Manager) devices
 	];
 	
 
@@ -27,5 +27,7 @@ in {
 
 
 }
+
+
 
 
