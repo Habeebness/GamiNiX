83ad666e1978d3b86c62c324a7ee02bb6003ee4f
iggut <igor.gutchin@gmail.com>
2023-04-06 11:29:45 -0400

    Update default.nix

--- system/desktop/hyprland/default.nix
+++ system/desktop/hyprland/default.nix
@@ -7,12 +7,12 @@
 	]; # Setup home manager for hyprland
 
 	programs = lib.mkIf config.desktop-environment.hyprland.enable {
-		nm-applet.enable = true; # Network manager tray icon
-		kdeconnect.enable = true; # Connect phone to PC
-	};
+    nm-applet.enable = true;       # Network manager tray icon
+    #kdeconnect.enable = true;      # Connect phone to PC
+  };
 
 	environment = lib.mkIf config.desktop-environment.hyprland.enable {
-		systemPackages = with pkgs; [
+	  systemPackages = with pkgs; [
 			# Status bar
 			(waybar.overrideAttrs (oldAttrs: {
 				mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
@@ -20,34 +20,35 @@
 					sed -i 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());/g' src/modules/wlr/workspace_manager.cpp
 				'';
 			}))
-			baobab # Disk usage analyser
-			blueberry # Bluetooth manager
-			clipman # Clipboard manager for wayland
-			dunst # Notification daemon
-			gnome.file-roller # Archive file manager
-			gnome.gnome-calculator # Calculator
-			gnome.gnome-disk-utility # Disks manager
-			gnome.gnome-themes-extra # Adwaita GTK theme
-			gnome.nautilus # File manager
-			grim # Screenshot tool
-			jq # JSON parser
-			playerctl # mpris controller
-			brightnessctl
-			pamixer
-			networkmanagerapplet # Network manager tray icon
-			pavucontrol # Sound manager
-			polkit_gnome # Polkit manager
-			rofi-wayland # App launcher
-			slurp # Monitor selector
-			swappy # Edit screenshots
-			wdisplays # Displays manager
-			wl-clipboard # Clipboard daemon
-			wlogout # Logout screen
-			swww # Wallpaper
-			eww-wayland #Widgets and bars
-			xdg-utils
-		];
-
+        baobab                      # Disk usage analyser
+        blueberry                   # Bluetooth manager
+        clipman                     # Clipboard manager for wayland
+        dunst                       # Notification daemon
+        gnome.file-roller           # Archive file manager
+        gnome.gnome-calculator      # Calculator
+        gnome.gnome-disk-utility    # Disks manager
+        gnome.gnome-themes-extra    # Adwaita GTK theme
+        gnome.nautilus              # File manager
+        grim                        # Screenshot tool
+        jq                          # JSON parser
+        playerctl                   # mpris controller
+        brightnessctl               # Adjust screen brightness
+        pamixer                     # PulseAudio mixer
+        networkmanagerapplet        # Network manager tray icon
+        pavucontrol                 # Sound manager
+        polkit_gnome                # Polkit manager
+        rofi-wayland                # App launcher
+        slurp                       # Monitor selector
+        swappy                      # Edit screenshots
+        wdisplays                   # Displays manager
+        wl-clipboard                # Clipboard daemon
+        wlogout                     # Logout screen
+        swww                        # Wallpaper
+        eww-wayland                 # Widgets and bars
+  	    xdg-utils                   # Utility commands for the desktop environment
+	  ];
+    
+		# Configure system services
 		etc = {
 			"wlogout-icons".source = "${pkgs.wlogout}/share/wlogout/icons";
 			"polkit-gnome".source = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
@@ -59,5 +60,5 @@
 		gvfs.enable = true; # Needed for nautilus
 	};
 
-	security.polkit.enable = lib.mkIf config.desktop-environment.hyprland.enable true;
+	security.polkit.enable = lib.mkIf config.desktop-environment.hyprland.enable true; # Enable polkit security
 }
