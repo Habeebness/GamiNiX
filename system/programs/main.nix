3f5acb822003a574193b3a722f43f21fa7f3337d
iggut <igor.gutchin@gmail.com>
2023-04-06 11:18:23 -0400

    Update main.nix

--- system/programs/main.nix
+++ system/programs/main.nix
@@ -10,49 +10,48 @@ lib.mkIf config.main.user.enable {
   nixpkgs.config.packageOverrides = pkgs: {
     steam = pkgs.steam.override {
       extraPkgs = pkgs: with pkgs; [
-        libgdiplus
-        zulu
-				gtk3
-        zlib
-        dbus
-        freetype
-        glib
-        atk
-        cairo
-				mono
-        gdk-pixbuf
-        pango
-        fontconfig
-				gnutls # needed for Halo MCC
-				stdenv.cc.cc.lib
-				gamescope # Wayland microcompositor
-				SDL2
-				icu63
-				libtensorflow
-				fontconfig
+		libgdiplus            # Mono library for GDI+ API
+        zulu                  # OpenJDK implementation
+        gtk3                  # Toolkit for creating graphical user interfaces
+        zlib                  # Compression/decompression library
+        dbus                  # Message bus system
+        freetype              # Font rendering engine
+        glib                  # Low-level core library
+        atk                   # Accessibility toolkit
+        cairo                 # 2D graphics library
+        mono                  # Open-source implementation of the .NET framework
+        gdk-pixbuf            # Image loading and manipulation library for GTK
+        pango                 # Library for layout and rendering of text
+        fontconfig            # Library for configuring and customizing font access
+        gnutls                # Transport Layer Security library needed for Halo MCC
+        stdenv.cc.cc.lib      # C++ library used by the Nix package manager
+        SDL2                  # Cross-platform development library for games and multimedia applications
+        icu63                 # Library for internationalization support
+        libtensorflow         # Library for machine learning and artificial intelligence
       ];
     };
   };
 
 	users.users.${config.main.user.username}.packages = with pkgs; lib.mkIf config.main.user.enable [
-		bottles # Wine manager
-		spotify # Viva la musica
-		duckstation # PS1 Emulator
-		godot_4 # Game engine
-		heroic # Epic Games Launcher for Linux
-		input-remapper # Remap input device controls
-		papermc # Minecraft server
-		pcsx2 # PS2 Emulator
-		ppsspp # PSP Emulator
-		prismlauncher # Minecraft launcher
-		protontricks # Winetricks for proton prefixes
-		rpcs3 # PS3 Emulator
-		ryujinx # Switch Emulator
-		scanmem # Cheat engine for linux
-		steamtinkerlaunch # General tweaks for games
-		stremio # Straming platform
-		sunshine # Remote gaming
-		prusa-slicer # 3D printer slicer software
+	bottles             # Wine manager
+    spotify             # Viva la musica
+    duckstation         # PS1 Emulator
+    godot_4             # Game engine
+    heroic              # Epic Games Launcher for Linux
+    input-remapper      # Remap input device controls
+    papermc             # Minecraft server
+    pcsx2               # PS2 Emulator
+    ppsspp              # PSP Emulator
+    prismlauncher       # Minecraft launcher
+    protontricks        # Winetricks for proton prefixes
+    rpcs3               # PS3 Emulator
+    ryujinx             # Switch Emulator
+    scanmem             # Cheat engine for Linux
+    steamtinkerlaunch   # General tweaks for games
+    stremio             # Streaming platform
+    sunshine            # Remote gaming
+    prusa-slicer        # 3D printer slicer software
+
 	];
 
 	services.input-remapper.enable = config.main.user.enable;
